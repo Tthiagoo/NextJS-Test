cbce2bf6d0120180ada45b66126e68834069a6ba
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2020-11-23 17:02:39 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
-export default function Home() {
-  return (
-    <h1>Hellow woorldd</h1>
-  )
+export default function Home({org}) {
+	return (
+		<div>
+			<h1>{org.login}</h1>
+			<h3>{org.description}</h3>
+
+			<p>
+				Site:<a href={org.blog}>{org.blog}</a>
+			</p>
+		</div>
+	);
 }
 
+export const getStaticProps = async () => {
+	const response = await fetch("https://api.github.com/orgs/rocketseat");
+  const data = await response.json();
+  console.log(data.login)
+	return {
+		props: {
+			org: data,
+		},
+	};
+};
